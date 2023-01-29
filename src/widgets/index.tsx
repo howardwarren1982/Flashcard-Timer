8b9fed87ecd19743faac43b44b34ead72a115ac0
howardwarren1982 <howardwarren89@gmail.com>
2023-01-28 22:55:54 -0500

    Deleted unused and test code

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -14,19 +14,6 @@ async function onActivate(plugin: ReactRNPlugin) {
   await plugin.app.registerWidget('sample_widget', WidgetLocation.RightSidebar, {
     dimensions: { height: 'auto', width: '100%' },
   });
-
-  plugin.event.addListener(AppEvents.URLChange, undefined, async ({ pathname }) => {
-    const url = await plugin.window.getURL();
-    if ((pathname as string).includes('/flashcards') || url.includes('/flashcards')) {
-      await plugin.window.openWidgetInRightSidebar('sample_widget', {});
-    }
-  });
-
-  const allrems = await plugin.rem.getAll();
-  const findone = await plugin.card.getAll();
-
-  console.log(allrems[0]._id);
-  console.log(findone);
 }
 
 async function onDeactivate(_: ReactRNPlugin) {}
